27d209340653666b7d9d333cd9f7f1153332a778
23765828 <23765828@sun.ac.za>
2023-05-07 20:20:00 +0200

    P2Pfile

---
+++ P2PfileTransfer/src/shared/EncryptionUtil.java
@@ -0,0 +1,11 @@
+package shared;
+
+public class EncryptionUtil {
+    public static String encrypt(String plainText) {
+        // Implement encryption logic
+    }
+
+    public static String decrypt(String cipherText) {
+        // Implement decryption logic
+    }
+}
