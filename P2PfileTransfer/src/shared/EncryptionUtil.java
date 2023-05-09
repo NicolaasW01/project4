c4a0c9dcd783ab3205e705e305395832f52bc805
23765828 <23765828@sun.ac.za>
2023-05-09 10:53:51 +0200

    Encryption

--- P2PfileTransfer/src/shared/EncryptionUtil.java
+++ P2PfileTransfer/src/shared/EncryptionUtil.java
@@ -1,39 +1,35 @@
 package shared;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-
 public class EncryptionUtil {
-    private static final String ALGORITHM = "AES";
-    private static final String KEY = "exampleKey123456"; // Replace with your own key
-
+    static int key = 6;
     public static String encrypt(String plainText) {
-        try {
-            SecretKeySpec keySpec = new SecretKeySpec(KEY.getBytes(StandardCharsets.UTF_8), ALGORITHM);
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-            byte[] encryptedBytes = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
-            return Base64.getEncoder().encodeToString(encryptedBytes);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+        char[] chars = plainText.toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        for (char c : chars) {
+            c += key;
+            result.append(c);
         }
+        //System.out.println(result.toString());
+        return result.toString();
+        
     }
 
     public static String decrypt(String cipherText) {
-        try {
-            byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
-            SecretKeySpec keySpec = new SecretKeySpec(KEY.getBytes(StandardCharsets.UTF_8), ALGORITHM);
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, keySpec);
-            byte[] decryptedBytes = cipher.doFinal(decodedBytes);
-            return new String(decryptedBytes, StandardCharsets.UTF_8);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+        char[] chars = cipherText.toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        for (char c : chars) {
+            c -= key;
+            result.append(c);
         }
+     //   System.out.println(result.toString());
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        decrypt("");
+
     }
 }
-
